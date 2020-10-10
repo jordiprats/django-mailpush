75871e8a4d4390801a5851f05fc7643549e703e9
Jordi Prats <jordi.prats@gmail.com>
2020-10-10 23:06:37 +0200

    skel owner

--- mailpush/mail/models.py
+++ mailpush/mail/models.py
@@ -1,9 +1,31 @@
+from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import AbstractUser
+from django.utils.text import slugify
 from django.db import models
 
-# Create your models here.
+import uuid
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    slug = models.SlugField(
+        default='',
+        editable=False,
+        max_length=100,
+        unique=True,
+    )
+    email = models.CharField(max_length=256)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.username, allow_unicode=False)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.email
 
 class Mail(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    subscription = models.ForeignKey(User, on_delete=models.CASCADE, related_name='delegatsclasses')
 
     from = models.EmailField(max_length=256)
     to = models.EmailField(max_length=256)
