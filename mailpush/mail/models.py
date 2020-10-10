eddf80480e59f1ff0452d7bebfd1f5d6613067c6
jordiprats <jordi.prats@gmail.com>
2020-10-10 23:00:43 +0200

    skel mail

--- mailpush/mail/models.py
+++ mailpush/mail/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Mail(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    from = models.EmailField(max_length=256)
+    to = models.EmailField(max_length=256)
+    bcc = models.EmailField(max_length=256)
+
+    subject = models.CharField(max_length=256)
+    text = models.TextField(null=True, default=None, blank=True)
+    html = models.TextField(null=True, default=None, blank=True)
